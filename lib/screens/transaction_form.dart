d52d3e6f181c1d658b668c86af3476be1664325a
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-06-18 15:18:55 -0300

    Corrigido a chamada de adição

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/http/webclients/transaction_webclient.dart';
 import 'package:flutter/material.dart';
 
 import '../models/contact.dart';
@@ -14,6 +15,7 @@ class TransactionForm extends StatefulWidget {
 
 class _TransactionFormState extends State<TransactionForm> {
   final TextEditingController _valueController = TextEditingController();
+  final TransactionWebClient _webClient = TransactionWebClient();
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +62,11 @@ class _TransactionFormState extends State<TransactionForm> {
                     child: Text('Transfer'), onPressed: () {
                       final double? value = double.tryParse(_valueController.text);
                       final transactionCreated = Transaction(value!, widget.contact);
+                      _webClient.save(transactionCreated).then( (transaction) {
+                        if(transaction != null){
+                          Navigator.pop(context);
+                        }
+                      });
                   },
                   ),
                 ),
