e77af97cad40b08263d0e276c35565ff34ae43e7
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-06-17 22:06:14 -0300

    Refatorado a lista de transacoes para suportar os estados de conexao e realizado tratamento de timeout e erros default

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,44 +1,60 @@
+import 'package:bytebank/components/centered_message.dart';
+import 'package:bytebank/http/webclient.dart';
 import 'package:flutter/material.dart';
 
+import '../components/progress.dart';
 import '../models/contact.dart';
 import '../models/transaction.dart';
 
 class TransactionsList extends StatelessWidget {
-  final List<Transaction> transactions = [];
-
   @override
   Widget build(BuildContext context) {
-    transactions.add(Transaction(100.0, Contact(0, 'Alex', 1000)));
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Transactions'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(
-                transaction.value.toString(),
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contact.accountNumber.toString(),
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Transactions'),
+        ),
+        body: FutureBuilder<List<Transaction>>(
+            future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  break;
+                case ConnectionState.waiting:
+                  return Progress();
+                  break;
+                case ConnectionState.active:
+                  break;
+                case ConnectionState.done:
+                  if(snapshot.hasData){
+                    final List<Transaction>? transactions = snapshot.data;
+                    return ListView.builder(itemBuilder: (context, index) {
+                      final Transaction transaction = transactions![index];
+                      return Card(
+                        child: ListTile(
+                          leading: Icon(Icons.monetization_on),
+                          title: Text(
+                            transaction.value.toString(),
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            transaction.contact.accountNumber.toString(),
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                      );
+                    }, itemCount: transactions!.length,);
+                  }
+
+                  return CenteredMessage('No transactions found', icon: Icons.warning,);
+
+                  break;
+              }
+              return CenteredMessage('Unknow error');
+            }));
   }
 }
-
-
