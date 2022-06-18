23c868273044dcb1982bf139069e3807a2ee70a2
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-06-17 22:05:45 -0300

    adicionado tratamento para a carga de dados conforme o retorno esperado do Future

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,21 +1,18 @@
-
-
+import 'package:bytebank/components/centered_message.dart';
+import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../database/dao/ContactDAO.dart';
 
 class ContactsList extends StatefulWidget {
-
   @override
   State<ContactsList> createState() => _ContactsListState();
 }
 
 class _ContactsListState extends State<ContactsList> {
-
-  final ContactDAO dao= ContactDAO();
+  final ContactDAO dao = ContactDAO();
 
   @override
   Widget build(BuildContext context) {
@@ -23,51 +20,49 @@ class _ContactsListState extends State<ContactsList> {
       appBar: AppBar(title: Text('Transfer')),
       body: FutureBuilder<List<Contact>>(
         // initialData: [], //insere um valor inicial
-        future: Future.delayed(Duration(seconds: 1)) //seta um delay na busca para simular uma chamada async
-            .then((value) => dao.findAll()),//busca todos os contatos
-        builder: (context, snapshot) { //quando tiver a resposta modifica o builder
-          switch(snapshot.connectionState){
+        future: Future.delayed(Duration(
+                seconds:
+                    1)) //seta um delay na busca para simular uma chamada async
+            .then((value) => dao.findAll()), //busca todos os contatos
+        builder: (context, snapshot) {
+          //quando tiver a resposta modifica o builder
+          switch (snapshot.connectionState) {
             case ConnectionState.none: //future ainda nao foi executado
               break;
-            case ConnectionState.waiting: //verifica que o future ainda esta carregando
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(),
-                    Text('Loading...')
-                  ],
-                ),
-              );
+            case ConnectionState
+                .waiting: //verifica que o future ainda esta carregando
+              Progress();
               break;
             case ConnectionState.active:
               //significa que tem dado disponivel mas ainda nao foi finalizado o future
               //usado por exemplo em download, mostrando o progresso continuo
               break;
             case ConnectionState.done:
-              if(snapshot.data != null){
+              if (snapshot.hasData) {
                 final List<Contact> contacts = snapshot.data as List<Contact>;
                 return ListView.builder(
-                  itemBuilder: (context, index){
+                  itemBuilder: (context, index) {
                     return _ContactItem(contacts[index]);
                   },
                   itemCount: contacts.length,
                 );
               }
+              CenteredMessage('Empty data', icon: Icons.error_rounded,);
+
               break;
           }
-          return Text('Unknown error');//provavelmente nunca caira nesse bloco
+          return CenteredMessage('Unknown error database'); //provavelmente nunca caira nesse bloco
         },
       ),
-
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           Navigator.of(context)
               .push(
-                MaterialPageRoute(builder: (context) => ContactForm(),
-              ),
-          ).then((value) => setState((){}));
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
+              .then((value) => setState(() {}));
         },
         child: Icon(Icons.add),
       ),
@@ -75,8 +70,7 @@ class _ContactsListState extends State<ContactsList> {
   }
 }
 
-class _ContactItem extends StatelessWidget{
-
+class _ContactItem extends StatelessWidget {
   final Contact contact;
 
   _ContactItem(this.contact);
@@ -85,13 +79,17 @@ class _ContactItem extends StatelessWidget{
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text(contact.name, style: TextStyle(fontSize: 24),),
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24),
+        ),
         subtitle: Text(contact.accountNumber.toString()),
-        onTap:(){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactForm(),));
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ContactForm(),
+          ));
         },
       ),
     );
   }
-
 }
