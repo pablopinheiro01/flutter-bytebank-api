d8e1f817ae061e3b9776ef63bc170788627ac981
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-06-21 21:20:36 -0300

    Inserido dialog

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/transaction_auth_dialog.dart';
 import 'package:bytebank/http/webclients/transaction_webclient.dart';
 import 'package:flutter/material.dart';
 
@@ -59,15 +60,27 @@ class _TransactionFormState extends State<TransactionForm> {
                 child: SizedBox(
                   width: double.maxFinite,
                   child: ElevatedButton(
-                    child: Text('Transfer'), onPressed: () {
-                      final double? value = double.tryParse(_valueController.text);
-                      final transactionCreated = Transaction(value!, widget.contact);
-                      _webClient.save(transactionCreated).then( (transaction) {
-                        if(transaction != null){
-                          Navigator.pop(context);
-                        }
-                      });
-                  },
+                    child: Text('Transfer'),
+                    onPressed: () {
+                      final double? value =
+                          double.tryParse(_valueController.text);
+                      final transactionCreated =
+                          Transaction(value!, widget.contact);
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return TransactionAuthDialog(
+                                onConfirm: (String password) {
+                              _webClient.save(transactionCreated, password).then(
+                                (transaction) {
+                                  if (transaction != null) {
+                                    Navigator.pop(context);
+                                  }
+                                },
+                              );
+                            });
+                          });
+                    },
                   ),
                 ),
               )
